04eabdaf1bd7846f237e3517f30dd245ccbc22f6
Kun_P <ampking98@outlook.com>
2017-12-11 07:48:18 +0800

    Update and rename Hello_World.R to Aliens.R

---
+++ Aliens.R
@@ -0,0 +1,4 @@
+cat("Hi, Mr/Ms  alient: I'm not sure if you can understand my message. 
+I'm a human from earth and I come in peace. By the time I'm writing this message 
+we have been desprately looking for another planet like earth but to no avail. 
+We are probably gone not, but just in case we will survive, Can we move to your place?")
